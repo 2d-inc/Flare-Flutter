3dce41b412682541fc3d06a4acbfb0a4378e60fb
Luigi Rosso <luigi.rosso@gmail.com>
2018-12-07 18:48:25 -0500

    Adding The Rocker (a cute penguin who loves dancing).

---
+++ example/penguin_dance/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare/math/mat2d.dart';
+import "package:flare_flutter/flare_actor.dart";
+import "package:flutter/material.dart";
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Rockin\' Penguin',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new MyHomePage(title: 'Rockin\' Penguin'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> implements FlareController {
+  double _rockAmount = 0.5;
+  double _speed = 1.0;
+  double _rockTime = 0.0;
+
+  ActorAnimation _rock;
+
+  void initialize(FlutterActorArtboard artboard) {
+    _rock = artboard.getAnimation("music_walk");
+  }
+
+  void setViewTransform(Mat2D viewTransform) {}
+
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    _rockTime += elapsed * _speed;
+    _rock.apply(_rockTime % _rock.duration, artboard, _rockAmount);
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: new AppBar(title: new Text(widget.title)),
+      body: new Stack(
+        children: [
+          Positioned.fill(
+              child: FlareActor("assets/Penguin.flr",
+                  alignment: Alignment.center,
+                  fit: BoxFit.cover,
+                  animation: "walk",
+                  controller: this)),
+          Positioned.fill(
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                Container(
+                    height: 100,
+                    color: Colors.black.withOpacity(0.5),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text("Mix Amount",
+                            style: TextStyle(color: Colors.white)),
+                        new Slider(
+                          value: _rockAmount,
+                          min: 0.0,
+                          max: 1.0,
+                          divisions: null,
+                          onChanged: (double value) {
+                            setState(() {
+                              _rockAmount = value;
+                            });
+                          },
+                        ),
+                        new Text("Speed",
+                            style: TextStyle(color: Colors.white)),
+                        new Slider(
+                          value: _speed,
+                          min: 0.2,
+                          max: 3.0,
+                          divisions: null,
+                          onChanged: (double value) {
+                            setState(() {
+                              _speed = value;
+                            });
+                          },
+                        )
+                      ],
+                    )),
+              ]))
+        ],
+      ),
+    );
+  }
+}

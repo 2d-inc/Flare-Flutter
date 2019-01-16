cb25f5edf7e741727c8f7507926ea2188e530e2f
Umberto Sonnino <rimher@gmail.com>
2019-01-16 11:05:26 +0100

    Add Robot demo too

--- example/slider/lib/main.dart
+++ example/slider/lib/main.dart
@@ -6,10 +6,8 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:slider/house_controller.dart';
-
-import 'demo_button_bar.dart';
-import 'house.dart';
-import 'robot.dart';
+import 'package:slider/demo_button_bar.dart';
+import 'package:slider/robot.dart';
 
 void main() {
   SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
@@ -48,6 +46,8 @@ class _StackPageState extends State<StackPage>
   String _selectedDemo = _barOptions[0];
   double _offset = 0.0;
 
+  Offset _touchPosition;
+
   AnimationController _sliderController;
   Animation<double> _slideAnimation;
 
@@ -59,6 +59,15 @@ class _StackPageState extends State<StackPage>
 
   _touchUp(PointerUpEvent details) {
     _scheduleDemo();
+  }
+
+  _setTouchPosition(PointerEvent details)
+  {
+    setState(() {
+      double dpr = window.devicePixelRatio;
+      Offset position = details.position;
+      _touchPosition = Offset(position.dx*dpr, position.dy*dpr);
+    });
   }
 
   _scheduleDemo() {
@@ -98,6 +107,21 @@ class _StackPageState extends State<StackPage>
             color: Colors.black,
             child: Stack(fit: StackFit.expand, children: [
               Positioned(
+                left: (_offset - 1) * -screenSize.width,
+                width: screenSize.width,
+                height: screenSize.height,
+                child: Listener(
+                  onPointerDown: _setTouchPosition,
+                  onPointerMove: _setTouchPosition,
+                  onPointerUp: (_) => _touchPosition = null,
+                  child: NimaWidget(
+                    "assets/nima/Robot_Kr2.nima",
+                    scrollOffset: -_offset+1,
+                    touchPosition: _touchPosition,
+                  ),
+                )
+              ),
+              Positioned(
                 left: _offset * -screenSize.width,
                 width: screenSize.width,
                 height: screenSize.height,
@@ -105,7 +129,7 @@ class _StackPageState extends State<StackPage>
                     onPointerUp: _touchUp,
                     child: Stack(children: [
                       FlareActor(
-                        "assets/Resizing_House.flr",
+                        "assets/flare/Resizing_House.flr",
                         fit: BoxFit.fill,
                         controller: _houseController,
                       ),
@@ -221,334 +245,5 @@ class _StackPageState extends State<StackPage>
                     ])
                   ])))
             ])));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  double _sliderValue = 3.0;
-  bool _isDemoMode = true;
-  Timer _currentDemoSchedule;
-  Object _selectedDemo = "DEMO 1";
-  double _offset = 0.0;
-  Offset _touchPosition;
-  bool _doubleTap = false;
-  int _lastTouch = 0;
-
-  AnimationController _controller;
-  Animation<double> _slideAnimation;
-
-  _MyHomePageState() {
-    _controller = new AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    _controller.addListener(() {
-      setState(() {
-        _offset = _slideAnimation.value;
-      });
-    });
-  }
-
-  void _scheduleDemo() {
-    if (_isDemoMode) {
-      return;
-    }
-    if (_currentDemoSchedule != null) {
-      _currentDemoSchedule.cancel();
-    }
-    _currentDemoSchedule = new Timer(const Duration(seconds: 5), () {
-      setState(() {
-        _isDemoMode = true;
-      });
-    });
-  }
-
-  void _demoValueChange(double rooms) {
-    setState(() {
-      _sliderValue = rooms - 3;
-    });
-  }
-
-  void _releaseScreen(PointerUpEvent details) {
-    _scheduleDemo();
-    setState(() {
-      _touchPosition = null;
-      _doubleTap = false;
-    });
-  }
-
-  void _touchScreen(PointerDownEvent details) {
-    setState(() {
-      int now = new DateTime.now().millisecondsSinceEpoch;
-      int diff = now - _lastTouch;
-      _touchPosition = new Offset(details.position.dx * window.devicePixelRatio,
-          details.position.dy * window.devicePixelRatio);
-      _doubleTap = diff < 500;
-      _lastTouch = now;
-    });
-    if (_currentDemoSchedule != null) {
-      _currentDemoSchedule.cancel();
-      _currentDemoSchedule = null;
-    }
-    _isDemoMode = false;
-  }
-
-  void _pointerMove(PointerMoveEvent details) {
-    setState(() {
-      _doubleTap = false;
-      _touchPosition = new Offset(details.position.dx * window.devicePixelRatio,
-          details.position.dy * window.devicePixelRatio);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            color: Colors.black,
-            child: Stack(fit: StackFit.expand, children: <Widget>[
-              NimaWidget("assets/nima/Robot_Kr2",
-                  scrollOffset: -_offset + 1.0, touchPosition: _touchPosition),
-              Listener(
-                  onPointerUp: _releaseScreen,
-                  onPointerMove: _pointerMove,
-                  onPointerDown: _touchScreen,
-                  child: Stack(children: <Widget>[
-                    FlareHouse(
-                        // flare: "assets/flares/HouseSky",
-                        flare: "assets/flares/Resizing_House.flr",
-                        rooms: (_sliderValue + 3).round(),
-                        isDemoMode: _isDemoMode,
-                        demoValueChange: _demoValueChange,
-                        offset: -_offset),
-                    OffsetStack(offset: -_offset, children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text((_sliderValue + 3).round().toString() + " ROOMS",
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 242, 242, 242),
-                                  fontFamily: "Roboto",
-                                  fontSize: 14.0,
-                                  decoration: TextDecoration.none,
-                                  fontWeight: FontWeight.w700)),
-                          Container(
-                            margin: EdgeInsets.only(
-                                top: 0.0, bottom: 0.0, left: 40.0, right: 40.0),
-                            child: Slider(
-                              value: _sliderValue,
-                              min: 0.0,
-                              max: 3.0,
-                              divisions: 3,
-                              onChanged: (double value) {
-                                setState(() {
-                                  _isDemoMode = false;
-                                  _sliderValue = value;
-
-                                  if (_currentDemoSchedule != null) {
-                                    _currentDemoSchedule.cancel();
-                                    _currentDemoSchedule = null;
-                                  }
-                                });
-                              },
-                            ),
-                          ),
-                          Text(
-                              _isDemoMode
-                                  ? "TAP TO TRY"
-                                  : "DRAG TO CHANGE ROOMS",
-                              style: TextStyle(
-                                  color: Color.fromARGB(128, 255, 255, 255),
-                                  fontFamily: "Roboto",
-                                  fontSize: 14.0,
-                                  decoration: TextDecoration.none,
-                                  fontWeight: FontWeight.w700)),
-                        ],
-                      )
-                    ])
-                  ])),
-              Container(
-                  margin: EdgeInsets.only(
-                      top: 0.0, bottom: 46.0, left: 40.0, right: 40.0),
-                  //color:Colors.black,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                          margin: const EdgeInsets.only(right: 4.0),
-                          width: 234.0,
-                          height: 18.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage("assets/images/2DLogo.png"),
-                                  fit: BoxFit.fitHeight,
-                                  alignment: Alignment.centerRight))),
-                      Container(
-                        margin: const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                        child: Text(
-                            "Powerful Realtime Animation for Apps, Games, and Web.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 255, 255, 255),
-                                fontFamily: "Roboto",
-                                fontSize: 18.0,
-                                decoration: TextDecoration.none,
-                                fontWeight: FontWeight.w100)),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(bottom: 40.0),
-                        child: DemoButtonBar(["DEMO 1", "DEMO 2"],
-                            selectedItem: _selectedDemo,
-                            selectedCallback: (int index, Object item) {
-                          _slideAnimation = Tween<double>(
-                                  begin: _offset, end: index.toDouble())
-                              .animate(_controller);
-
-                          _controller
-                            ..value = 0.0
-                            ..fling(velocity: 0.5);
-
-                          setState(() {
-                            _selectedDemo = item;
-                          });
-                        }),
-                      ),
-                      Row(children: <Widget>[
-                        Text("2DIMENSIONS.COM",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 255, 255, 255),
-                                fontFamily: "Roboto",
-                                fontSize: 14.0,
-                                decoration: TextDecoration.none,
-                                fontWeight: FontWeight.w700)),
-                        Expanded(
-                            child: Container(
-                                margin: const EdgeInsets.only(right: 4.0),
-                                width: 32.0,
-                                height: 32.0,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: AssetImage(
-                                            "assets/images/flutter.png"),
-                                        fit: BoxFit.fitHeight,
-                                        alignment: Alignment.centerRight)))),
-                        Text("FLUTTER",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 255, 255, 255),
-                                fontFamily: "Roboto",
-                                fontSize: 14.0,
-                                decoration: TextDecoration.none,
-                                fontWeight: FontWeight.w700))
-                      ])
-                    ],
-                  ))
-            ])));
-  }
-}
-
-class OffsetStack extends MultiChildRenderObjectWidget {
-  final double offset;
-  OffsetStack({
-    Key key,
-    this.offset,
-    List<Widget> children: const <Widget>[],
-  }) : super(key: key, children: children);
-
-  @override
-  RenderOffsetStack createRenderObject(BuildContext context) {
-    return new RenderOffsetStack(offset);
-  }
-
-  @override
-  void updateRenderObject(
-      BuildContext context, covariant RenderOffsetStack renderObject) {
-    renderObject.offset = offset;
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder description) {
-    super.debugFillProperties(description);
-  }
-}
-
-class RenderOffsetStackParentData extends ContainerBoxParentData<RenderBox> {}
-
-class RenderOffsetStack extends RenderBox
-    with
-        ContainerRenderObjectMixin<RenderBox, RenderOffsetStackParentData>,
-        RenderBoxContainerDefaultsMixin<RenderBox,
-            RenderOffsetStackParentData> {
-  double _offset = 0.0;
-
-  RenderOffsetStack(double _offset) {
-    this.offset = offset;
-  }
-
-  double get offset {
-    return _offset;
-  }
-
-  set offset(double v) {
-    if (_offset == v) {
-      return;
-    }
-    _offset = v;
-
-    markNeedsLayout();
-  }
-
-  @override
-  bool get sizedByParent => true;
-
-  @override
-  void performResize() {
-    size = constraints.biggest;
-  }
-
-  @override
-  void setupParentData(RenderBox child) {
-    if (child.parentData is! RenderOffsetStackParentData) {
-      child.parentData = new RenderOffsetStackParentData();
-    }
-  }
-
-  @override
-  void performLayout() {
-    // For now, just place them in a grid. Later we need to use MaxRects to figure out the best layout as some cells will be double height.
-    RenderBox child = firstChild;
-
-    int idx = 0;
-    while (child != null) {
-      Constraints constraints = new BoxConstraints(
-          minWidth: size.width,
-          maxWidth: size.width,
-          minHeight: size.height,
-          maxHeight: size.height);
-      child.layout(constraints, parentUsesSize: true);
-      final RenderOffsetStackParentData childParentData = child.parentData;
-      childParentData.offset = new Offset(_offset * size.width, 0.0);
-      child = childParentData.nextSibling;
-      idx++;
-    }
-  }
-
-  @override
-  bool hitTestChildren(HitTestResult result, {Offset position}) {
-    return defaultHitTestChildren(result, position: position);
-  }
-
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    defaultPaint(context, offset);
   }
 }

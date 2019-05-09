4e9961b6ca9023194744ac320c4e4d5988a6532f
Luigi Rosso <luigi.rosso@gmail.com>
2019-05-09 11:43:49 -0700

    Update indices to 16 bit unsigned, compatibility with flutter master.

--- flare_flutter/lib/flare.dart
+++ flare_flutter/lib/flare.dart
@@ -911,7 +911,7 @@ class FlutterActorImage extends ActorImage with FlutterActorDrawable {
   Float32List _uvBuffer;
   ui.Paint _paint;
   ui.Vertices _canvasVertices;
-  Int32List _indices;
+  Uint16List _indices;
 
   void onPaintUpdated(ui.Paint paint) {}
   final Float64List _identityMatrix = Float64List.fromList(<double>[
@@ -971,8 +971,7 @@ class FlutterActorImage extends ActorImage with FlutterActorDrawable {
     }
     _vertexBuffer = makeVertexPositionBuffer();
     _uvBuffer = makeVertexUVBuffer();
-    _indices =
-        Int32List.fromList(triangles); // nima runtime loads 16 bit indices
+    _indices = triangles;
     updateVertexUVBuffer(_uvBuffer);
     int count = vertexCount;
     int idx = 0;

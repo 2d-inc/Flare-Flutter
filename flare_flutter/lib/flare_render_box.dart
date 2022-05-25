d84775217d91ae9d0d39ab6ea80e29343203954f
amrnrd <amrmohamed257@gmail.com>
2022-05-25 11:03:56 +0200

    remove safe navigation operator from SchedulerBinding.instance to support flutter 3.0

--- flare_flutter/lib/flare_render_box.dart
+++ flare_flutter/lib/flare_render_box.dart
@@ -164,10 +164,10 @@ abstract class FlareRenderBox extends RenderBox {
     if (isPlaying) {
       // Paint again
       if (_frameCallbackID != -1) {
-        SchedulerBinding.instance?.cancelFrameCallbackWithId(_frameCallbackID);
+        SchedulerBinding.instance.cancelFrameCallbackWithId(_frameCallbackID);
       }
       _frameCallbackID =
-          SchedulerBinding.instance?.scheduleFrameCallback(_beginFrame) ?? -1;
+          SchedulerBinding.instance.scheduleFrameCallback(_beginFrame) ?? -1;
     }
 
     final Canvas canvas = context.canvas;
@@ -269,7 +269,7 @@ abstract class FlareRenderBox extends RenderBox {
     } else {
       _lastFrameTime = _notPlayingFlag;
       if (_frameCallbackID != -1) {
-        SchedulerBinding.instance?.cancelFrameCallbackWithId(_frameCallbackID);
+        SchedulerBinding.instance.cancelFrameCallbackWithId(_frameCallbackID);
       }
     }
   }
